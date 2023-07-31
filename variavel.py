10f15b24861f5ef7de2a7d4e0fbe8bf1526317d3
Felipe Augusto Machado Sampaio Miguel <felipeasmiguel@gmail.com>
2023-07-31 11:12:15 -0300

    Variavel criada.

---
+++ variavel.py
@@ -0,0 +1,8 @@
+# O "input" aguarda uma resposta do usuário
+'''
+Outro
+Comentário
+'''
+nome = input("Qual é o seu nome?")
+
+print("Olá,", nome, ", Este é um programa feito para testar o git e entender alguns conceitos iniciais de Python")
